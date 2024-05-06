17e76f81004080acfe35f1a0e3054aace82e5a10
Xiong Zeng <zengxion@umich.edu>
2024-05-06 11:56:39 -0400

    1

--- K_L_G_computation.m
+++ K_L_G_computation.m
@@ -8,26 +8,40 @@ m = size(C_all,2);
 K_all = zoers(N,p,n);
 L_all = zoers(N,n,m);
 
+G_cl_all = zeros(N,N,m,h*p);
+
 for i = 1:N
-    [K_all(i,:,:),S1,e1] = dlqr(A_all(i,:,:),B_all(i,:,:),Q,R);
-    [K_temp,S2,e2] = dlqr(A_all(i,:,:)',C_all(i,:,:)',sigma_w * eye(n),sigma_z * eye(p)) ;
+    A = squeeze(A_all(i,:,:));
+    B = squeeze(B_all(i,:,:));
+    C = squeeze(C_all(i,:,:));
+    [K_all(i,:,:),S1,e1] = dlqr(A, B, Q,R);
+    [K_temp,S2,e2] = dlqr(A',C',sigma_w * eye(n),sigma_z * eye(p)) ;
     L_all(i,:,:) = K_temp';
 end
 
  
 for i = 1:N
+    A_h = squeeze(A_all(i,:,:));
+    B_h = squeeze(B_all(i,:,:));
+    C_h = squeeze(C_all(i,:,:));
+    K_h = squeeze(K_all(i,:,:));
+    L_h = squeeze(L_all(i,:,:));
     for j=1:N
-        G = zeros(m,h*p);
-        %A_h = [A-B*K B*K;zeros(n,n) A-L*C];
-        A_h = [A -B*K; L*C A-L*C-B*K];
-        %B_h = [B;0];
-        B_h = [B;B];
-        C_h = [C zeros(m,n)];
+        A = squeeze(A_all(j,:,:));
+        B = squeeze(B_all(j,:,:));
+        C = squeeze(C_all(j,:,:));
+        A_t = [A-B*K_h   B*K_h; ...
+            (A_h-A)+(B_h-B)*K_h+L_h*(C-C_h)   A_h-L_h*C_h+(B_h-B)*K_h];
+        B_t = [B;zeros(size(B))];
+        C_t = [C zeros(size(C))];
 
-        G(:,1:p) = D;
+        G_cl_all(i,j,:,1:p) = D;
         for k = 1:T-1
-            G(:,k*p + 1: (k+1)*p) = C*A^(k-1)*B;
+            G_cl_all(i,j,:,k*p + 1: (k+1)*p) = C_t*A_t^(k-1)*B_t;
         end
+
+        %find the critical direction
+        
     end
 end
 
--- main.m
+++ main.m
@@ -22,24 +22,3 @@ sigma_z = 0.02;
 [A_all,B_all,C_all] = similar_system_generation(r,m,n,p,N);
 [K_all,L_all,G_cl_all] = K_L_G_computation(A_all,B_all,C_all,Q,R,sigma_w,sigma_z,h);
  
-%A bug in the paper is that r is not determined by your assumption and it
-%is determined by your controllers and your system properties.
-
-
-% 
-% [K_hat,S1_hat,e1_hat] = dlqr(A_hat,B_hat,Q,R) ;
-% [K_temp_hat,S2_hat,e2_hat] = dlqr(A_hat',C_hat',sigma_w * eye(n),sigma_z * eye(p)) ;
-% L_hat = K_temp_hat';
-% 
-% eig(A-B*K_hat)
-% eig(A-L_hat*C)
-% 
-% %A_h = [A-B*K B*K;zeros(n,n) A-L*C];
-% A_h_hat = [A -B*K_hat; L_hat*C A_hat-L_hat*C_hat-B_hat*K_hat];
-% %B_h = [B;0];
-% B_h_hat = [B;B_hat];
-% C_h_hat = [C zeros(m,n)];
-% Ob_h_hat = obsv(A_h_hat,C_h_hat);
-% Co_h_hat = ctrb(A_h_hat,B_h_hat);
-% rank(Ob_h_hat)
-% rank(Co_h_hat)
