3316b4180a41aba7991c7eee8dbe401d667794d6
Xiong Zeng <zengxion@umich.edu>
2024-05-06 00:33:08 -0400

    1

--- main.m
+++ main.m
@@ -2,15 +2,15 @@ r = 1.1; %r is the spetral radius of A
 m = 1; %m is the output dimension
 n = 3; %n is the system state dimension
 p = 1; %p is the control input dimension
-N =5; % the number of system candidates
-h=5; % The length of the time horizon of Markov parameter matrix 
+N = 5; % the number of system candidates
+h = 5; % The length of the time horizon of Markov parameter matrix 
 
 A_all = zeros(N,n,n);
 B_all = zeros(N,n,p );
 C_all = zeros(N,m,n );
-G_closed_loop_all = zeros(N,N,m,T*p);
-K_LQG_all = zoers(N,p,n);
-L_LQG_all = zoers(N,n,m);
+K_all = zoers(N,p,n);
+L_all = zoers(N,n,m);
+G_cl_all = zeros(N,N,m,h*p);
 
 Q = eye(n);
 R = eye(p);
@@ -20,9 +20,8 @@ sigma_w = 0.02;
 sigma_z = 0.02;
 
 [A_all,B_all,C_all] = similar_system_generation(r,m,n,p,N);
-[K_LQG_all,L_LQG_all] = K_L_computation(A_all,B_all,C_all,Q,R,sigma_w,sigma_z);
-G_closed_loop_all = similar_G_generation(A_all,B_all,C_all,K_LQG_all,L_LQG_all);
-
+[K_all,L_all,G_cl_all] = K_L_G_computation(A_all,B_all,C_all,Q,R,sigma_w,sigma_z,h);
+ 
 %A bug in the paper is that r is not determined by your assumption and it
 %is determined by your controllers and your system properties.
 
