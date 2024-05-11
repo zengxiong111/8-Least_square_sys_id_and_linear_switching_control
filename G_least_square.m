a9186b31809ea689d8cc1dea8a41ed30c403f693
Xiong Zeng <zengxion@umich.edu>
2024-05-11 16:48:41 -0400

    1

--- G_least_square.m
+++ G_least_square.m
@@ -5,15 +5,18 @@ N_hat = size(U_single,1);
 
 N = N_hat - h;
 
+m = size(Y_single,2);
+p = size(U_single,2);
+
 
 Y = zeros(N,m);
-U = zeros(N,T*p);
+U = zeros(N,h*p);
 
 for i = 1:N
-    Y(i,:) = Y_single(:,i+T-1);
-    u_bar = U_single(:,i+T-1);
-    for j = 1:T-1
-        u_bar = [u_bar;U_single(:,i+T-1 - j)];
+    Y(i,:) = Y_single(:,i+h-1);
+    u_bar = U_single(:,i+h-1);
+    for j = 1:h-1
+        u_bar = [u_bar;U_single(:,i+h-1 - j)];
     end
     U(i,:) = u_bar';
 end
