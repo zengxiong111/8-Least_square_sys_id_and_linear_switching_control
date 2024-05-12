8050c52d219c00644eedc62320052bb6e9400374
Xiong Zeng <zengxion@umich.edu>
2024-05-12 17:09:21 -0400

    1

--- closed_loop_construction_trail.m
+++ closed_loop_construction_trail.m
@@ -1,3 +1,5 @@
+clear
+
 %r is the spetral radius of A
 %m is the output dimension
 %n is the system state dimension
@@ -46,3 +48,33 @@ rank(Ob_h)
 svd_Ob_h = svd(Ob_h)
 svd(obsv(A,C))
 rank(Co_h)
+
+
+
+function [A,B,C,D] = system_generation(r,m,n,p)
+
+A = zeros(n,n);
+
+B = zeros(n,p);
+C = zeros(m,n);
+
+D = zeros(m,p);
+
+B = normrnd(0,1/n,[n,p]);
+C = normrnd(0,1/m,[m,n]);
+
+A = rand(n);
+A = r * A/vrho(A);
+
+% A = [r 0.5;0 r];
+% B = [0;1];
+% C = [1 0];
+% D = [0 0];
+% 
+% A = [1 0.01 0;0.01 1 0.01;0 0.01 1];
+% A = r*A/vrho(A);
+% B=eye(3);
+% C = [1 0 0];
+% D = [0 0 0];
+
+end
